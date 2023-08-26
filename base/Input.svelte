df2a89e05e1894f8191dc6ac9c8fed539bc77d13
Fevol <vernieuwefelix@gmail.com>
2023-08-27 00:35:17 +0200

    fix: change event of input not correctly triggering, missing spinner definition

--- base/Input.svelte
+++ base/Input.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-	export let type: string;
+	import type { HTMLInputTypeAttribute } from 'svelte/elements';
+
+	export let type: HTMLInputTypeAttribute;
 	export let value: string;
 	export let placeholder: string = '';
-	export let onChange: (value: string) => void;
+	export let onChange: (value: string) => void = () => {};
 	export let valid: boolean = false;
 	export let readonly: boolean = false;
 </script>
 
-
+<!--TODO: value keyword is still an issue-->
 <input
 	{type}
 	{value}
 	{placeholder}
 	{readonly}
-	on:input={() => onChange(value)}
+	on:input={(e) => onChange(e.target.value)}
 	class={$$props.class}
 	class:svelcomlib-input-success={valid}
 	class:svelcomlib-input-fail={valid === false}
